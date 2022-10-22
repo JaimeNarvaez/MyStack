c2692de4481db054f0dbc8e72aefeaa298a37a6d
JaimeNarvaez <jaime.narvaez.quintanilla@gmail.com>
2022-10-21 20:57:00 -0600

    Project created on eclipse ide

---
+++ src/stk/CharStack.java
@@ -0,0 +1,108 @@
+package stk;
+
+public class CharStack {
+
+	int p;
+	int t;
+
+	int r;
+	char[] stk;
+
+	char e;
+
+	public CharStack() {
+		init(32);
+
+	}
+
+	public CharStack(int k) {
+		init(k);
+	}
+
+	private void init(int v) {
+		p = 0;
+		t = 0;
+
+		r = 0;
+		stk = new char[v];
+
+		e = ' ';
+
+		t = stk.length;
+
+	}
+
+	public void push(char n) {
+
+		if (p == t) {
+			/** to resize **/
+		} else if (p < t) {
+
+			if (p == -1) {
+				p = 0;
+			}
+
+			stk[p] = n;
+			p++;
+		}
+
+	}
+
+	public char pop() {
+
+		if (p < t) {
+
+			if (p > 0) {
+				p = p - 1;
+				r = p;
+				r = r - 1;
+
+				if (r > 0) {
+					e = stk[p];
+					stk[r] = e;
+					stk[p] = '\u0000';
+
+				} else if (r == 0) {
+					e = stk[r];
+					stk[r] = '\u0000';
+				}
+
+			} else if (p == 0) {
+				e = stk[p];
+				p = -1;
+
+			}
+
+		} else if (p == t) {
+			p = p - 1;
+			e = stk[p];
+
+		}
+
+		return e;
+
+	}
+
+	public char peek() {
+
+		if (p < t) {
+
+			if (p > 0) {
+			} else if (p == -1) {
+
+			}
+
+		} else if (p == t) {
+		}
+
+		return stk[p];
+	}
+
+	public static void main(String[] args) {
+		CharStack st;
+
+		st = new CharStack();
+
+	}
+
+}
